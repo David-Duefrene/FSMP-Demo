f92180d275aa6b8aa59ffe48c87fe72abefce623
David Duefrene <1645298+David-Duefrene@users.noreply.github.com>
2023-09-29 22:09:59 -0600

    initial router
    initial router

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import App from './Containers/Home/Home.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import Home from './Containers/Home/Home.tsx'
 
 import './index.css'
 
+const router = createBrowserRouter([
+	{ path: '/', element: <Home /> },
+])
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<App />
+		<RouterProvider router={router} />
 	</React.StrictMode>,
 )
